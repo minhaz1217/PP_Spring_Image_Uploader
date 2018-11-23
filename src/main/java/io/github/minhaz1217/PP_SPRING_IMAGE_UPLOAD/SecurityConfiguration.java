ea4dd743777a4f2cfa6fc704141a6abae7f504ee
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-11-23 21:52:08 +0600

    added custom users instead of the generated password

---
+++ src/main/java/io/github/minhaz1217/PP_SPRING_IMAGE_UPLOAD/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package io.github.minhaz1217.PP_SPRING_IMAGE_UPLOAD;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .permitAll();
+    }
+    @Autowired
+    public void configureInMemoryUsers(AuthenticationManagerBuilder auth) throws Exception {
+        User.withDefaultPasswordEncoder().username("user").password("user").roles("USER").build();
+
+
+        /*
+
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("user").password("password").roles("USER");
+*/
+
+/*
+        User.withUsername("greg").password("temp").roles("ADMIN", "USER");
+        User.withUsername("myuser").password("myuser").roles("USER");
+        User.withUsername("123").password("123").roles("USER").disabled(true);
+        User.withUsername("asd").password("asd").roles("USER").accountLocked(true);
+        */
+
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("greg").password("temp").roles("ADMIN", "USER")
+        .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("myuser").password("myuser").roles("USER")
+        .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("123").password("123").roles("USER").disabled(true)
+        .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("asd").password("asd").roles("USER").accountLocked(true);
+
+    }
+}
